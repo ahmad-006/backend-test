6d03c925f510b94d49522ed06814321334665cbc
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-09-17 22:48:08 +0500

    fix: fixed all bugs and ready for deployment

--- models/userModel.js
+++ models/userModel.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
       required: [true, "Please provide a password"],
       minlength: 8,
     },
-    
+
     avatar: {
       type: String,
       trim: true,
